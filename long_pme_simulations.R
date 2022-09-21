35c8017c475c5738cf9d4099b1a46a5464f86779
Rob Zielinski <robert_zielinski1@brown.edu>
2022-09-21 11:41:07 -0400

    update simulations

--- long_pme_simulations.R
+++ long_pme_simulations.R
@@ -41,8 +41,47 @@ sim_D2d1_case1 <- function(time_val, vertical_multiplier, horizontal_multiplier,
   return(data.points)
 }
 
-time_vals <- seq(0, 2, 0.25)
+sim_D2d1_case2 <- function(time_val, vertical_multiplier, horizontal_multiplier, noise, time_noise) { 
+  I <- 1000
+  t <- rnorm(I, mean = 0, sd = 1)
+  horizontal_noise <- rnorm(1, mean = 0, sd = time_noise)
+  vertical_noise <- rnorm(1, mean = 0, sd = time_noise)
+  sd.noise <- noise
+  e1 <- rnorm(I, mean = 0, sd = sd.noise)
+  e2 <- rnorm(I, mean = 0, sd = sd.noise)
+  X <- matrix(NA, nrow = I, ncol = 2)
+  manifold <- function(tau, time_val, vertical_multiplier, horizontal_multiplier, vertical_noise, horizontal_noise) {
+    return(
+      c(
+        # cos(tau) + (horizontal_multiplier * sin(time_val)),
+        cos(tau) + horizontal_noise,
+        # sin(tau) + (vertical_multiplier * sin(time_val))
+        sin(tau) + vertical_noise
+      )
+    )
+  }
+  
+  X <- map(
+    t, 
+    ~ manifold(
+      .x, 
+      time_val, 
+      vertical_multiplier, 
+      horizontal_multiplier, 
+      vertical_noise, 
+      horizontal_noise
+    )
+  ) %>% 
+    unlist() %>% 
+    matrix(ncol = 2, byrow = TRUE)
+  data.points <- X + cbind(e1, e2)
+  data.points <- cbind(time_val, data.points)
+  return(data.points)
+}
 
+time_vals <- 0:5
+
+set.seed(100)
 df_list <- lapply(
   time_vals, 
   sim_D2d1_case1, 
