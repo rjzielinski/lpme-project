037a18762e397e62f27844167a87e2e153f5c27e
Rob Zielinski <robert_zielinski1@brown.edu>
2022-09-21 11:37:21 -0400

    example updates

--- Examples_for_PME.R
+++ Examples_for_PME.R
@@ -194,22 +194,26 @@ t=cbind(t.1,t.2)
 data.points=t(apply(t,1,manifold))+cbind(e1,e2,e3)
 
 ptm <- proc.time()
-result=PME(x.obs=data.points, d=2)
+result_test=PME(x.obs=data.points, d=2)
 proc.time() - ptm
 
-f=result$embedding.map
+# f <- result_test$embedding.map
+f <- sim_result$embedding_map
 t.plot.1=seq(from=-2, to=2, length.out = 200)
 t.plot.2=seq(from=-2, to=2, length.out = 200)
 t.length=length(t.plot.1)
 surf.plot=matrix(0,ncol=3,nrow=1)
-for(i in 1:t.length){
-  for(j in 1:t.length){
-    surf.plot=rbind(surf.plot,f(c(t.plot.1[i],t.plot.2[j])))
-  }
-}
-x.test=surf.plot[-1,]
-index=(x.test[,1]>=min(data.points[,1]))&x.test[,1]<=max(data.points[,1])&(x.test[,2]>=min(data.points[,2]))&x.test[,2]<=max(data.points[,2])&(x.test[,3]>=min(data.points[,3]))&x.test[,3]<=max(data.points[,3])
-scatter3D(data.points[,1], data.points[,2], data.points[,3], 
+surf.plot <- t(apply(expand_grid(t.plot.1, t.plot.2), 1, f))
+# for(i in 1:t.length){
+#   print(i)
+#   for(j in 1:t.length){
+#     surf.plot=rbind(surf.plot,f(c(t.plot.1[i],t.plot.2[j])))
+#   }
+# }
+# x.test=surf.plot[-1,]
+x.test <- surf.plot
+index=(x.test[,1]>=min(data_points[,1]))&x.test[,1]<=max(data_points[,1])&(x.test[,2]>=min(data_points[,2]))&x.test[,2]<=max(data_points[,2])&(x.test[,3]>=min(data_points[,3]))&x.test[,3]<=max(data_points[,3])
+scatter3D(data_points[,1], data_points[,2], data_points[,3], 
           pch = 20, box=FALSE, cex = 0.5, colkey = FALSE, 
           border="black", shade=0.8, 
           ticktype = "detailed",
@@ -218,5 +222,45 @@ scatter3D(x.test[index,1], x.test[index,2],x.test[index,3],
           pch = 20, box=FALSE, cex = 0.1, colkey = FALSE, col = "grey", 
           border="black", shade=0.8, main=" ",add = TRUE)
 
+x_df <- data.frame(x.test[index, ])
+x_df <- cbind("manifold", x_df)
+names(x_df) <- c(
+  "type",
+  "x",
+  "y",
+  "z"
+)
 
+# data_df <- data.frame(data.points_long)
+data_df <- data.frame(data_points)
+data_df <- cbind("data", data_df)
+names(data_df) <- c(
+  "type",
+  "x",
+  "y", 
+  "z"
+)
 
+data_df <- bind_rows(data_df, x_df)
+
+fig <- plot_ly(
+  data_df,
+  x = ~x,
+  y = ~y,
+  z = ~z,
+  type = "scatter3d",
+  color = ~type
+) # %>% 
+  add_surface(
+    x = x_df$x,
+    y = x_df$y,
+    z = x_df$z
+  )
+
+fig2 <- plot_ly(
+  x_df,
+  x = ~x,
+  y = ~y,
+  z = ~z
+) %>% 
+  add_surface()
