20950b7282b3f379d09c3aa48927506acdc7f58e
Rob Zielinski <robert_zielinski1@brown.edu>
2022-09-24 23:33:25 -0400

    run lpme for D2d1 case 2

--- long_pme_simulations.R
+++ long_pme_simulations.R
@@ -146,3 +146,70 @@ plot_ly(
   type = "scatter3d",
   mode = "markers"
 )
+
+time_vals <- 0:10
+
+set.seed(100)
+df_list <- lapply(
+  time_vals, 
+  sim_D2d1_case2, 
+  vertical_multiplier = 1.5, 
+  horizontal_multiplier = 1.5, 
+  noise = 0.15, 
+  time_noise = 1
+)
+
+data_points <- reduce(df_list, rbind)
+
+sim_result <- long_pme(data_points, 1)
+
+# profvis({
+#   sim_result <- long_pme(data_points, 1)
+# })
+
+time_vals <- seq(0, 3, 0.1)
+r_vals <- seq(-10, 10, 0.1)
+grid_mat <- expand_grid(time_vals, r_vals)
+
+sim_pred <- matrix(nrow = nrow(grid_mat), ncol = ncol(grid_mat))
+for (i in 1:nrow(sim_pred)) {
+  sim_pred[i, ] <- sim_result$embedding_map(unlist(as.vector(grid_mat[i, ])))
+}
+
+idx_inrange <- matrix(nrow = dim(sim_pred)[1], ncol = dim(sim_pred)[2])
+for (dim_idx in 1:dim(sim_pred)[2]) {
+  idx_range <- max(data_points[, dim_idx + 1]) - min(data_points[, dim_idx + 1])
+  idx_min <- min(data_points[, dim_idx + 1]) - (0.2 * idx_range)
+  idx_max <- max(data_points[, dim_idx + 1]) + (0.2 * idx_range)
+  idx_inrange[, dim_idx] <- (sim_pred[, dim_idx] > idx_min) & 
+    (sim_pred[, dim_idx] < idx_max)
+}
+    
+r_inrange <- rowSums(idx_inrange) == dim(sim_pred)[2]
+r_min <- min(unlist(grid_mat[, 2][r_inrange, 1]))
+r_max <- max(unlist(grid_mat[, 2][r_inrange, 1]))
+r_vals <- seq(
+  r_min,
+  r_max,
+  0.1
+)
+
+grid_mat <- expand_grid(time_vals, r_vals)
+
+sim_pred <- matrix(nrow = nrow(grid_mat), ncol = ncol(grid_mat))
+for (i in 1:nrow(sim_pred)) {
+  sim_pred[i, ] <- sim_result$embedding_map(unlist(as.vector(grid_mat[i, ])))
+}
+
+sim_pred_full <- cbind(grid_mat, sim_pred)
+sim_pred_full_df <- data.frame(sim_pred_full)
+names(sim_pred_full_df) <- c("time", "r", "x", "y")
+
+plot_ly(
+  sim_pred_full_df,
+  x = ~x,
+  y = ~y,
+  z = ~time,
+  type = "scatter3d",
+  mode = "markers"
+)
