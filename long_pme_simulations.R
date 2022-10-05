9963671711b02367cb849db5b056f951eca66e39
Rob Zielinski <robert_zielinski1@brown.edu>
2022-10-05 10:04:12 -0400

    update D3d2 case 1

--- long_pme_simulations.R
+++ long_pme_simulations.R
@@ -3,6 +3,7 @@ library(plotly)
 library(profvis)
 
 source("lpme.R")
+source("pme.R")
 
 sim_D2d1_case1 <- function(time_val, vertical_multiplier, horizontal_multiplier, noise, time_noise) { 
   I <- 1000
@@ -236,7 +237,9 @@ sim_D3d1_case2 <- function(time_val, vertical_multiplier, horizontal_multiplier,
 
 sim_D3d2_case1 <- function(time_val, vertical_multiplier, horizontal_multiplier, depth_multiplier, noise, time_noise) { 
   I <- 1000
-  t <- runif(I, min = 0, max = 3 * pi)
+  t1 <- runif(I, min = -1, max = 1)
+  t2 <- runif(I, min = -1, max = 1)
+  t <- cbind(t1, t2)
   horizontal_noise <- rnorm(1, mean = 0, sd = time_noise)
   vertical_noise <- rnorm(1, mean = 0, sd = time_noise)
   depth_noise <- rnorm(1, mean = 0, sd = time_noise)
@@ -248,25 +251,24 @@ sim_D3d2_case1 <- function(time_val, vertical_multiplier, horizontal_multiplier,
   manifold <- function(tau, time_val, vertical_multiplier, horizontal_multiplier, depth_multiplier, vertical_noise, horizontal_noise, depth_noise) {
     return(
       c(
-        tau + (horizontal_multiplier * sin(time_val)) + horizontal_noise,
-        cos(tau) + (vertical_multiplier * sin(time_val)) + vertical_noise,
-        sin(tau) + (depth_multiplier * sin(time_val)) + depth_noise
+        tau[1] + (horizontal_multiplier * sin(time_val)) + horizontal_noise,
+        tau[2] + (vertical_multiplier * sin(time_val)) + vertical_noise,
+        (norm_euclidean(tau) ^ 2) + (depth_multiplier * sin(time_val)) + depth_noise
       )
     )
   }
   
-  X <- map(
+  X <- apply(
     t, 
-    ~ manifold(
-      .x, 
-      time_val, 
-      vertical_multiplier, 
-      horizontal_multiplier,
-      depth_multiplier,
-      vertical_noise, 
-      horizontal_noise,
-      depth_noise
-    )
+    1,
+    manifold,
+    time_val = time_val, 
+    vertical_multiplier = vertical_multiplier, 
+    horizontal_multiplier = horizontal_multiplier,
+    depth_multiplier = depth_multiplier,
+    vertical_noise = vertical_noise,
+    horizontal_noise = horizontal_noise,
+    depth_noise = depth_noise
   ) %>% 
     unlist() %>% 
     matrix(ncol = 3, byrow = TRUE)
@@ -679,3 +681,72 @@ plot_ly(
   mode = "markers"
 )
 
+### D3, d2 Simulation Case 1
+
+time_vals <- 0:3
+
+set.seed(100)
+df_list <- lapply(
+  time_vals, 
+  sim_D3d2_case1, 
+  vertical_multiplier = 1,
+  horizontal_multiplier = 1,
+  depth_multiplier = 1,
+  noise = 0.15, 
+  time_noise = 1
+)
+
+data_points <- reduce(df_list, rbind)
+
+sim_result <- lpme(data_points, 2)
+
+time_vals <- seq(0, 5, 0.1)
+r_vals <- seq(-10, 10, 0.1)
+grid_mat <- expand_grid(time_vals, r_vals)
+
+sim_pred <- matrix(nrow = nrow(grid_mat), ncol = ncol(grid_mat))
+for (i in 1:nrow(sim_pred)) {
+  sim_pred[i, ] <- sim_result$embedding_map(unlist(as.vector(grid_mat[i, ])))
+}
+
+idx_inrange <- matrix(nrow = dim(sim_pred)[1], ncol = dim(sim_pred)[2])
+for (dim_idx in 1:dim(sim_pred)[2]) {
+  idx_range <- max(data_points[, dim_idx + 1]) - min(data_points[, dim_idx + 1])
+  idx_min <- min(data_points[, dim_idx + 1]) - (0.2 * idx_range)
+  idx_max <- max(data_points[, dim_idx + 1]) + (0.2 * idx_range)
+  idx_inrange[, dim_idx] <- (sim_pred[, dim_idx] > idx_min) & 
+    (sim_pred[, dim_idx] < idx_max)
+}
+    
+r_inrange <- rowSums(idx_inrange) == dim(sim_pred)[2]
+r_min <- min(unlist(grid_mat[, 2][r_inrange, 1]))
+r_max <- max(unlist(grid_mat[, 2][r_inrange, 1]))
+if (sum(r_inrange) == 0) {
+  r_min <- -10
+  r_max <- 10
+}
+r_vals <- seq(
+  r_min,
+  r_max,
+  0.1
+)
+
+grid_mat <- expand_grid(time_vals, r_vals)
+
+sim_pred <- matrix(nrow = nrow(grid_mat), ncol = ncol(grid_mat))
+for (i in 1:nrow(sim_pred)) {
+  sim_pred[i, ] <- sim_result$embedding_map(unlist(as.vector(grid_mat[i, ])))
+}
+
+sim_pred_full <- cbind(grid_mat, sim_pred)
+sim_pred_full_df <- data.frame(sim_pred_full)
+names(sim_pred_full_df) <- c("time", "r", "x", "y")
+
+plot_ly(
+  sim_pred_full_df,
+  x = ~x,
+  y = ~y,
+  z = ~time,
+  type = "scatter3d",
+  mode = "markers"
+)
