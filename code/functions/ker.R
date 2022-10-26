4f24ee9ee7a549c29bde96242b0712ecad6eb49b
Rob Zielinski <robert_zielinski1@brown.edu>
2022-10-25 23:45:58 -0400

    optimize ker code

--- code/functions/ker.R
+++ code/functions/ker.R
@@ -1,5 +1,6 @@
 ker <- function(x, mu, sigma) {
   # Smoothing kernel for density estimation
-  yseq <- sapply((x - mu) / sigma, dnorm)
+  #yseq <- sapply((x - mu) / sigma, dnorm)
+  yseq <- dnorm((x - mu) / sigma)
   return((sigma ^ {-length(x)}) * prod(yseq))
 }
