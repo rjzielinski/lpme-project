1c76f7bac68bfcf7266bedff77195109bc538164
Rob Zielinski <robert_zielinski1@brown.edu>
2022-10-25 23:46:28 -0400

    remove commented code

--- code/functions/ker.R
+++ code/functions/ker.R
@@ -1,6 +1,5 @@
 ker <- function(x, mu, sigma) {
   # Smoothing kernel for density estimation
-  #yseq <- sapply((x - mu) / sigma, dnorm)
   yseq <- dnorm((x - mu) / sigma)
   return((sigma ^ {-length(x)}) * prod(yseq))
 }
