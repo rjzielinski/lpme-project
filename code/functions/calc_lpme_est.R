58638019201640a67adb90d4af19adcc58755e62
rjzielinski <robert_zielinski1@brown.edu>
2023-03-22 08:05:34 -0400

    use new projection function in error calculations

--- code/functions/calc_lpme_est.R
+++ code/functions/calc_lpme_est.R
@@ -1,11 +1,10 @@
 calc_lpme_est <- function(lpme, df) {
   clusters <- lpme$clusters
   opt_run <- which.min(lpme$MSD)
-  tnew_init <- lpme$TNEW[[opt_run]][clusters, ] %>%
-    matrix(nrow = nrow(df), byrow = TRUE)
+  tnew_init <- lpme$TNEW[[opt_run]][clusters, ]
   tnew <- map(
     1:nrow(df),
-    ~ projection(df[.x, ], lpme$embedding_map, tnew_init[.x, ])
+    ~ projection_lpme(df[.x, ], lpme$embedding_map, tnew_init[.x, ])
   ) %>%
     reduce(rbind)
   results <- map(
