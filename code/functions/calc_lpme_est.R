94e3ceb46813574bac41b16d2387a03fe4748e58
rjzielinski <robert_zielinski1@brown.edu>
2023-04-05 13:46:10 -0400

    update for new version of lpme

--- code/functions/calc_lpme_est.R
+++ code/functions/calc_lpme_est.R
@@ -1,15 +1,28 @@
-calc_lpme_est <- function(lpme, df) {
-  clusters <- lpme$clusters
-  opt_run <- which.min(lpme$MSD)
-  tnew_init <- lpme$TNEW[[opt_run]][clusters, ]
+calc_lpme_est <- function(result, df) {
+  opt_idx <- which.min(result$MSD)
+  x_vals <- result$x_vals[[opt_idx]]
+  tnew_init <- result$TNEW[[opt_idx]]
+
+  nearest_x <- map(
+    1:nrow(df),
+    ~ which.min(apply(x_vals[x_vals[, 1] == df[.x, 1], ], 1, dist_euclideanC, y = df[.x, ]))
+  ) %>%
+    reduce(c)
+
+  init_param <- map(
+    1:nrow(df),
+    ~ tnew_init[tnew_init[, 1] == df[.x, 1], ][nearest_x[.x], ]
+  ) %>%
+    reduce(rbind)
+
   tnew <- map(
     1:nrow(df),
-    ~ projection_lpme(df[.x, ], lpme$embedding_map, tnew_init[.x, ])
+    ~ projection_lpme(df[.x, ], result$embedding_map, init_param[.x, ])
   ) %>%
     reduce(rbind)
   results <- map(
     1:nrow(df),
-    ~ lpme$embedding_map(tnew[.x, ])
+    ~ result$embedding_map(tnew[.x, ])
   ) %>%
     reduce(rbind)
   return(results)
