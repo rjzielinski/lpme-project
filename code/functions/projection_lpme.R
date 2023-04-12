a1873529f712c594ac4d1bbfa5d394dd4ea65c17
rjzielinski <robert_zielinski1@brown.edu>
2023-04-12 15:06:54 -0400

    in case of error, rerun nlm function

--- code/functions/projection_lpme.R
+++ code/functions/projection_lpme.R
@@ -5,19 +5,33 @@ projection_lpme <- function(x, f, initial.guess) {
       p = initial.guess[-1]
     )
   )
+  count <- 1
   if (class(nlm_est) == "try-error") {
-    opts <- list("algorithm" = "NLOPT_LN_COBYLA", "xtol_rel" = 1e-04)
-    nlopt_est <- try(
-      nloptr(
-        x0 = initial.guess,
-        function(t) dist_euclideanC(x = x, f(t)),
-        opts = opts
+    while (class(nlm_est) == "try-error" & count < 10) {
+      nlm_est <- try(
+        nlm(
+          function(t) dist_euclideanC(x = x, f(matrix(c(initial.guess[1], t), nrow = 1))),
+          p = initial.guess[-1]
+        )
       )
-    )
-    if (class(nlopt_est) == "try-error") {
-      return(NA)
+      count <- count + 1
+    }
+    if (class(nlm_est) != "try-error") {
+      return(c(initial.guess[1], nlm_est$estimate))
     } else {
-      return(c(initial.guess[1], nlopt_est$solution))
+      opts <- list("algorithm" = "NLOPT_LN_COBYLA", "xtol_rel" = 1e-04)
+      nlopt_est <- try(
+        nloptr(
+          x0 = initial.guess,
+          function(t) dist_euclideanC(x = x, f(t)),
+          opts = opts
+        )
+      )
+      if (class(nlopt_est) == "try-error") {
+        return(NA)
+      } else {
+        return(c(initial.guess[1], nlopt_est$solution))
+      }
     }
   } else {
     return(c(initial.guess[1], nlm_est$estimate))
