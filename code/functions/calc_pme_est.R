449c0d31215cac88a85d268459244c54448c5bea
rjzielinski <robert_zielinski1@brown.edu>
2023-06-15 22:21:13 -0400

    update for pme() and lpme() restructuring

--- code/functions/calc_pme_est.R
+++ code/functions/calc_pme_est.R
@@ -1,19 +1,19 @@
 calc_pme_est <- function(pme, df) {
   clusters <- pme$knots$cluster
   opt_run <- which.min(pme$MSD)
-  tnew_init <- pme$TNEW[[opt_run]][clusters, ] %>%
+  tnew_init <- pme$parameterization[[opt_run]][clusters, ] %>%
     matrix(nrow = nrow(df), byrow = TRUE)
-  sol_init <- pme$SOL[[opt_run]][clusters, ] %>%
+  sol_init <- pme$coefs[[opt_run]][clusters, ] %>%
     matrix(nrow = nrow(df), byrow = TRUE)
 
   tnew <- map(
     1:nrow(df),
-    ~ projection(df[.x, ], pme$embedding.map, tnew_init[.x, ])
+    ~ projection_pme(df[.x, ], pme$embedding_map, tnew_init[.x, ])
   ) %>%
     reduce(rbind)
   results <- map(
     1:nrow(df),
-    ~ pme$embedding.map(tnew[.x, ])
+    ~ pme$embedding_map(tnew[.x, ])
   ) %>%
     reduce(rbind)
   return(results)
