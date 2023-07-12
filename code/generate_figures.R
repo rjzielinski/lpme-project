25857846b19e0b90dd8c7229b0edc11c86398ed7
rjzielinski <robert_zielinski1@brown.edu>
2023-07-12 08:56:05 -0400

    update simulation case 1 figure

--- code/generate_figures.R
+++ code/generate_figures.R
@@ -60,9 +60,9 @@ lines3D(
 )
 dev.off()
 
-sim_example_case1 <- readRDS("simulations/case1/duration_05_interval_010_ampnoise_125_pernoise_000_n_1000_constant025_run_03.rds")
-sim_example_case5 <- readRDS("simulations/case5/duration_05_interval_100_ampnoise_175_pernoise_125_n_1000_quadratic000_run_04.rds")
-sim_example_case8 <- readRDS("simulations/case8/duration_02_interval_025_ampnoise_125_pernoise_025_n_1000_sinusoidal200_run_02.rds")
+sim_example_case1 <- readRDS("simulations/case1/duration_01_interval_010_ampnoise_050_pernoise_050_n_1000_constant000_run_01.rds")
+sim_example_case5 <- readRDS("simulations/case5/duration_01_interval_010_ampnoise_050_pernoise_000_n_10000_constant000_run_01.rds")
+sim_example_case7 <- readRDS("simulations/case7/duration_01_interval_010_ampnoise_050_pernoise_050_n_1000_constant000_run_01.rds")
 
 df_labels <- c("Data", "True", "LPME", "PME", "Principal Curve")
 
@@ -82,10 +82,10 @@ for (df_idx in 1:length(data_case1)) {
 }
 df_case1 <- reduce(data_case1, rbind)
 df_case1 <- df_case1 %>%
-  filter(mod(time, 1) == 0)
+  filter(mod(time * 10, 2) == 0)
 
 time_vals <- sim_example_case1$times
-time_vals <- time_vals[mod(time_vals, 1) == 0]
+time_vals <- time_vals[mod(time_vals * 10, 2) == 0]
 
 png("paper/figures/sim_case1.png")
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
@@ -132,6 +132,8 @@ for (time_idx in 1:length(time_vals)) {
     ylab = "y",
     col = alpha("black", 0.4),
     bg = alpha("black", 0.4),
+    xlim = c(-4, 4),
+    ylim = c(-3, 3),
     pch = 21,
     main = paste0("Time = ", time_vals[time_idx])
   )
@@ -195,14 +197,17 @@ for (df_idx in 1:length(data_case5)) {
 }
 df_case5 <- reduce(data_case5, rbind)
 df_case5 <- df_case5 %>%
-  filter(mod(time, 1) == 0)
+  filter(mod(time * 10, 2) == 0)
 
 time_vals <- sim_example_case5$times
-time_vals <- time_vals[mod(time_vals, 1) == 0]
+time_vals <- time_vals[mod(time_vals * 10, 2) == 0]
 
 png("paper/figures/sim_case5.png")
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
 for (time_idx in 1:length(time_vals)) {
+  temp_df <- df_case5 %>%
+    filter(time == time_vals[time_idx]) %>%
+    arrange(x)
   temp_data <- df_case5 %>%
     filter(
       time == time_vals[time_idx],
@@ -239,49 +244,57 @@ for (time_idx in 1:length(time_vals)) {
   # temp_princurve <- sim_example_case1$principal_curve_vals[temp_data[, 1] == time_vals[time_idx], ]
   # temp_true <- sim_example_case1$true_vals[temp_data[, 1] == time_vals[time_idx], ]
 
-  plt <- scatterplot3d(
+  plt <- scatter3D(
     x = temp_data$x,
     y = temp_data$y,
     z = temp_data$z,
     xlab = "x",
     ylab = "y",
     zlab = "z",
+    xlim = c(-2, 2),
+    ylim = c(0, 10),
+    zlim = c(0, 20),
     pch = 21,
-    color = alpha("black", 0.5),
-    bg = alpha("black", 0.5),
-    main = paste0("Time = ", time_vals[time_idx])
+    col = alpha("black", 0.01),
+    bg = alpha("black", 0.01),
+    main = paste0("Time = ", time_vals[time_idx]),
+    ticktype = "detailed"
   )
-  plt$points3d(
+  scatter3D(
     x = temp_true$x,
     y = temp_true$y,
     z = temp_true$z,
     col = alpha("red", 0.5),
     bg = alpha("red", 0.5),
-    pch = 21
+    pch = 20,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_lpme$x,
     y = temp_lpme$y,
     z = temp_lpme$z,
     col = "blue",
     bg = "blue",
-    pch = 21
+    pch = 20,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_pme$x,
     y = temp_pme$y,
     z = temp_pme$z,
     col = "green",
     bg = "green",
-    pch = 21
+    pch = 20,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_princurve$x,
     y = temp_princurve$y,
     z = temp_princurve$z,
     col = "purple",
     bg = "purple",
-    pch = 21
+    pch = 20,
+    add = TRUE
   )
 }
 par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
@@ -299,16 +312,16 @@ legend(
 )
 dev.off()
 
-data_case8 <- list(
-  sim_example_case8$df,
-  sim_example_case8$true_vals,
-  sim_example_case8$lpme_vals,
-  sim_example_case8$pme_vals,
-  sim_example_case8$principal_curve_vals
+data_case7 <- list(
+  sim_example_case7$df,
+  sim_example_case7$true_vals,
+  sim_example_case7$lpme_vals,
+  sim_example_case7$pme_vals,
+  sim_example_case7$principal_curve_vals
 )
 
-for (df_idx in 1:length(data_case8)) {
-  df <- as.data.frame(data_case8[[df_idx]])
+for (df_idx in 1:length(data_case7)) {
+  df <- as.data.frame(data_case7[[df_idx]])
   if (dim(df)[2] == 4) {
     names(df) <- c("time", "x", "y", "z")
   } else if (dim(df)[2] == 5) {
@@ -316,21 +329,21 @@ for (df_idx in 1:length(data_case8)) {
   }
   df$type <- df_labels[df_idx]
   if ("r" %in% names(df)) {
-    data_case8[[df_idx]] <- dplyr::select(df, -r)
+    data_case7[[df_idx]] <- dplyr::select(df, -r)
   } else {
-    data_case8[[df_idx]] <- df
+    data_case7[[df_idx]] <- df
   }
 }
-df_case8 <- reduce(data_case8, rbind)
-df_case8 <- df_case8 %>%
-  filter(mod(time * 2, 1) == 0)
+df_case7 <- reduce(data_case7, rbind)
+df_case7 <- df_case7 %>%
+  filter(mod(time * 10, 2) == 0)
 
-time_vals <- unique(df_case8$time)
+time_vals <- unique(df_case7$time)
 
-png("paper/figures/sim_case8.png")
+png("paper/figures/sim_case7.png")
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
 for (time_idx in 1:length(time_vals)) {
-  temp_data <- df_case8 %>%
+  temp_data <- df_case7 %>%
     filter(
       time == time_vals[time_idx],
       type == "Data"
@@ -338,25 +351,25 @@ for (time_idx in 1:length(time_vals)) {
     arrange(x)
   # temp_data <- temp_data[temp_data[, 1] == time_vals[time_idx], ]
   # temp_lpme <- sim_example_case1$lpme_vals[temp_data[, 1] == time_vals[time_idx], ]
-  temp_lpme <- df_case8 %>%
+  temp_lpme <- df_case7 %>%
     filter(
       time == time_vals[time_idx],
       type == "LPME"
     ) %>%
     arrange(x)
-  temp_pme <- df_case8 %>%
+  temp_pme <- df_case7 %>%
     filter(
       time == time_vals[time_idx],
       type == "PME"
     ) %>%
     arrange(x)
-  temp_princurve <- df_case8 %>%
+  temp_princurve <- df_case7 %>%
     filter(
       time == time_vals[time_idx],
       type == "Principal Curve"
     ) %>%
     arrange(x)
-  temp_true <- df_case8 %>%
+  temp_true <- df_case7 %>%
     filter(
       time == time_vals[time_idx],
       type == "True"
@@ -366,7 +379,7 @@ for (time_idx in 1:length(time_vals)) {
   # temp_princurve <- sim_example_case1$principal_curve_vals[temp_data[, 1] == time_vals[time_idx], ]
   # temp_true <- sim_example_case1$true_vals[temp_data[, 1] == time_vals[time_idx], ]
 
-  plt <- scatterplot3d(
+  plt <- scatter3D(
     x = temp_data$x,
     y = temp_data$y,
     z = temp_data$z,
@@ -374,41 +387,45 @@ for (time_idx in 1:length(time_vals)) {
     ylab = "y",
     zlab = "z",
     pch = 21,
-    color = alpha("black", 0.5),
-    bg = alpha("black", 0.5),
+    col = alpha("black", 0.05),
+    bg = alpha("black", 0.05),
     main = paste0("Time = ", time_vals[time_idx])
   )
-  plt$points3d(
+  scatter3D(
     x = temp_true$x,
     y = temp_true$y,
     z = temp_true$z,
     col = alpha("red", 0.5),
     bg = alpha("red", 0.5),
-    pch = 21
+    pch = 21,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_lpme$x,
     y = temp_lpme$y,
     z = temp_lpme$z,
     col = "blue",
     bg = "blue",
-    pch = 21
+    pch = 21,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_pme$x,
     y = temp_pme$y,
     z = temp_pme$z,
     col = "green",
     bg = "green",
-    pch = 21
+    pch = 21,
+    add = TRUE
   )
-  plt$points3d(
+  scatter3D(
     x = temp_princurve$x,
     y = temp_princurve$y,
     z = temp_princurve$z,
     col = "purple",
     bg = "purple",
-    pch = 21
+    pch = 21,
+    add = TRUE
   )
 }
 par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
