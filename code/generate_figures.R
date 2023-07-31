12184098295d7e430e72b5e1bdef665f9474269d
rjzielinski <robert_zielinski1@brown.edu>
2023-07-31 16:14:47 -0400

    update figure legends

--- code/generate_figures.R
+++ code/generate_figures.R
@@ -87,7 +87,7 @@ df_case1 <- df_case1 %>%
 time_vals <- sim_example_case1$times
 time_vals <- time_vals[mod(time_vals * 10, 2) == 0]
 
-png("paper/figures/sim_case1.png", res = 500, height = 2000, width = 2000)
+png("paper/figures/sim_case1.png", res = 500, height = 2000, width = 3000)
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
 for (time_idx in 1:length(time_vals)) {
   temp_data <- df_case1 %>%
@@ -179,14 +179,15 @@ for (time_idx in 1:length(time_vals)) {
     lwd = 1.25
   )
 }
-par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+par(fig = c(0, 1, 0, 1), oma = c(0, 1, 0, 1), mar = c(0, 0, 0, 0), new = TRUE)
 plot(0, 0, type = "l", bty = "n", xaxt = "n", yaxt = "n")
 legend(
   "bottom",
   c("Data", "True Manifold", "LPME", "PME", "Principal Curve"),
   col = c("black", "red", "blue", "green", "purple"),
   # fill = c("black", "red", "blue", "green", "purple"),
-  lwd = 5,
+  lty = c("solid", "solid", "dashed", "dotted", "dotdash"),
+  lwd = 1.25,
   xpd = TRUE,
   horiz = TRUE,
   cex = 1,
@@ -215,7 +216,7 @@ df_case5 <- df_case5 %>%
 time_vals <- sim_example_case5$times
 time_vals <- time_vals[mod(time_vals * 10, 2) == 0]
 
-png("paper/figures/sim_case5.png", res = 500, height = 2000, width = 2000)
+png("paper/figures/sim_case5.png", res = 500, height = 2000, width = 3000)
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
 for (time_idx in 1:length(time_vals)) {
   temp_df <- df_case5 %>%
@@ -280,7 +281,8 @@ for (time_idx in 1:length(time_vals)) {
     col = alpha("red", 0.5),
     bg = alpha("red", 0.5),
     pch = 20,
-    cex = 0.33,
+    type = "l",
+    lwd = 1,
     add = TRUE
   )
   scatter3D(
@@ -290,7 +292,9 @@ for (time_idx in 1:length(time_vals)) {
     col = "blue",
     bg = "blue",
     pch = 20,
-    cex = 0.33,
+    type = "l",
+    lty = 2,
+    lwd = 1,
     add = TRUE
   )
   scatter3D(
@@ -300,7 +304,9 @@ for (time_idx in 1:length(time_vals)) {
     col = "green",
     bg = "green",
     pch = 20,
-    cex = 0.33,
+    type = "l",
+    lty = 3,
+    lwd = 1,
     add = TRUE
   )
   scatter3D(
@@ -310,7 +316,9 @@ for (time_idx in 1:length(time_vals)) {
     col = "purple",
     bg = "purple",
     pch = 20,
-    cex = 0.33,
+    type = "l",
+    lty = 4,
+    lwd = 1,
     add = TRUE
   )
 }
@@ -321,7 +329,7 @@ legend(
   c("Data", "True Manifold", "LPME", "PME", "Principal Curve"),
   col = c("black", "red", "blue", "green", "purple"),
   # fill = c("black", "red", "blue", "green", "purple"),
-  lwd = 5,
+  lwd = 1,
   xpd = TRUE,
   horiz = TRUE,
   cex = 1,
@@ -357,7 +365,7 @@ df_case7 <- df_case7 %>%
 
 time_vals <- unique(df_case7$time)
 
-png("paper/figures/sim_case7.png", res = 500, height = 2000, width = 2000)
+png("paper/figures/sim_case7.png", res = 500, height = 2000, width = 3000)
 par(oma = c(4, 1, 1, 1), mfrow = c(2, 3), mar = c(2, 2, 1, 1))
 for (time_idx in 1:length(time_vals)) {
   temp_data <- df_case7 %>%
@@ -409,47 +417,47 @@ for (time_idx in 1:length(time_vals)) {
     pch = 21,
     col = alpha("black", 0.05),
     bg = alpha("black", 0.05),
-    cex = 0.33,
+    cex = 0.1,
     main = paste0("Time = ", time_vals[time_idx])
   )
   scatter3D(
     x = temp_true$x,
     y = temp_true$y,
     z = temp_true$z,
-    col = alpha("red", 0.5),
-    bg = alpha("red", 0.5),
+    col = alpha("red", 0.25),
+    bg = alpha("red", 0.25),
     pch = 21,
-    cex = 0.33,
+    cex = 0.1,
     add = TRUE
   )
   scatter3D(
     x = temp_lpme$x,
     y = temp_lpme$y,
     z = temp_lpme$z,
-    col = "blue",
-    bg = "blue",
+    col = alpha("blue", 0.25),
+    bg = alpha("blue", 0.25),
     pch = 21,
-    cex = 0.33,
+    cex = 0.1,
     add = TRUE
   )
   scatter3D(
     x = temp_pme$x,
     y = temp_pme$y,
     z = temp_pme$z,
-    col = "green",
-    bg = "green",
+    col = alpha("green", 0.25),
+    bg = alpha("green", 0.25),
     pch = 21,
-    cex = 0.33,
+    cex = 0.1,
     add = TRUE
   )
   scatter3D(
     x = temp_princurve$x,
     y = temp_princurve$y,
     z = temp_princurve$z,
-    col = "purple",
-    bg = "purple",
+    col = alpha("purple", 0.25),
+    bg = alpha("purple", 0.25),
     pch = 21,
-    cex = 0.33,
+    cex = 0.1,
     add = TRUE
   )
 }
