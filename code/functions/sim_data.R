6c7b6fec08fc8296d168ccd41352ecac806868a6
Robert Zielinski <rjz1996@outlook.com>
2024-07-10 16:48:52 -0400

    update case numbers

--- code/functions/sim_data.R
+++ code/functions/sim_data.R
@@ -25,22 +25,15 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
         amp_noise[1] * cos(period_noise[1] * tau),
         amp_noise[2] * sin(period_noise[2] * tau)
       ) +
-      time_change_d2
-    },
-    function(tau, amp_noise, period_noise) {
-      c(
-        amp_noise[1] * cos(period_noise[1] * tau),
-        amp_noise[2] * sin(period_noise[2] * tau)
-      ) +
-      time_change_d2
+        time_change_d2
     },
     function(tau, amp_noise, period_noise) {
       c(
         tau,
-        (tau * amp_noise[1] + period_noise[1]) ^ 2,
-        (tau * amp_noise[2] + period_noise[2]) ^ 3
+        (tau * amp_noise[1] + period_noise[1])^2,
+        (tau * amp_noise[2] + period_noise[2])^3
       ) +
-      time_change_d3
+        time_change_d3
     },
     function(tau, amp_noise, period_noise) {
       c(
@@ -48,15 +41,15 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
         amp_noise[1] * cos(period_noise[1] * tau),
         amp_noise[2] * sin(period_noise[2] * tau)
       ) +
-      time_change_d3
+        time_change_d3
     },
     function(tau, amp_noise, period_noise) {
       c(
         (period_noise[1] / 2) * tau[1],
         (period_noise[2] / 2) * tau[2],
-        (amp_noise[1]) * ((amp_noise[2]) * norm_euclidean(period_noise[1:d] * tau) ^ 2)
+        (amp_noise[1]) * ((amp_noise[2]) * norm_euclidean(period_noise[1:d] * tau)^2)
       ) +
-      time_change_d3
+        time_change_d3
     },
     function(tau, amp_noise, period_noise) {
       c(
@@ -64,22 +57,13 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
         (period_noise[2] * amp_noise[2] * tau[1]) * sin(amp_noise[2] * tau[1]),
         tau[2]
       ) +
-      time_change_d3
+        time_change_d3
     },
     function(tau, amp_noise, period_noise) {
       c(
         amp_noise[1] * sin(period_noise[1] * tau[1]) * cos(period_noise[2] * tau[2]),
         amp_noise[1] * sin(period_noise[1] * tau[1]) * sin(period_noise[2] * tau[2]),
         amp_noise[1] * cos(period_noise[1] * tau[1])
-      ) +
-      time_change_d3
-    },
-    function(tau, amp_noise, period_noise) {
-      r <- 1 + amp_noise[1] * (tau[1] + 1) * sqrt(tau[2] + 1)
-      c(
-        r * sin(period_noise[1] * tau[1]) * cos(period_noise[2] * tau[2]),
-        r * sin(period_noise[1] * tau[1]) * sin(period_noise[2] * tau[2]),
-        r * cos(period_noise[1] * tau[1])
       ) +
         time_change_d3
     }
@@ -89,13 +73,11 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
     case == 1 ~ 2,
     case == 2 ~ 2,
     case == 3 ~ 2,
-    case == 4 ~ 2,
+    case == 4 ~ 3,
     case == 5 ~ 3,
     case == 6 ~ 3,
     case == 7 ~ 3,
-    case == 8 ~ 3,
-    case == 9 ~ 3,
-    case == 10 ~ 3
+    case == 8 ~ 3
   )
   d <- case_when(
     case == 1 ~ 1,
@@ -103,11 +85,9 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
     case == 3 ~ 1,
     case == 4 ~ 1,
     case == 5 ~ 1,
-    case == 6 ~ 1,
+    case == 6 ~ 2,
     case == 7 ~ 2,
-    case == 8 ~ 2,
-    case == 9 ~ 2,
-    case == 10 ~ 2
+    case == 8 ~ 2
   )
 
   manifold <- manifolds[[case]]
@@ -125,21 +105,16 @@ sim_data <- function(time_val, case, noise, amp_noise, period_noise, time_change
   } else if (case == 3) {
     t[, 1] <- runif(I, min = -0.8 * pi, max = 0.5 * pi)
   } else if (case == 4) {
-    t[, 1] <- runif(I, time_val * (pi / 4), (time_val + 5) * (pi / 4))
-  } else if (case == 5) {
     t[, 1] <- runif(I, min = -1, max = 1)
-  } else if (case == 6) {
+  } else if (case == 5) {
     t[, 1] <- runif(I, min = 0, max = 3 * pi)
-  } else if (case == 7) {
+  } else if (case == 6) {
     t[, 1] <- runif(I, min = -1, max = 1)
     t[, 2] <- runif(I, min = -1, max = 1)
-  } else if (case == 8) {
+  } else if (case == 7) {
     t[, 1] <- runif(I, min = (0.2 * pi), max = (2.9 * pi))
     t[, 2] <- runif(I, min = -1, max = 1)
-  } else if (case == 9) {
-    t[, 1] <- runif(I, min = 0, max = pi)
-    t[, 2] <- runif(I, min = 0, max = 2 * pi)
-  } else if (case == 10) {
+  } else if (case == 8) {
     t[, 1] <- runif(I, min = 0, max = pi)
     t[, 2] <- runif(I, min = 0, max = 2 * pi)
   }
