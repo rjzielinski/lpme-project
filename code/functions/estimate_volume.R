070407f9c99b7877ffb7fee627749df47b862eee
Robert Zielinski <rjz1996@outlook.com>
2025-02-21 10:48:01 -0500

    updated sphere volume estimation

--- code/functions/estimate_volume.R
+++ code/functions/estimate_volume.R
@@ -4,299 +4,77 @@ estimate_volume <- function(data, voxel_volume) {
   indices <- matrix(ncol = 3)
   data_limits <- colMinsMaxs(data)
 
-  x_seq <- seq(data_limits[1, 1], data_limits[2, 1])
-  y_seq <- seq(data_limits[1, 2], data_limits[2, 2])
-  z_seq <- seq(data_limits[1, 3], data_limits[2, 3])
+  x_seq <- seq(data_limits[1, 1], data_limits[2, 1], by = 0.1)
+  y_seq <- seq(data_limits[1, 2], data_limits[2, 2], by = 0.1)
+  z_seq <- seq(data_limits[1, 3], data_limits[2, 3], by = 0.1)
 
-  # fill_candidates <- expand.grid(x_seq, y_seq, z_seq)
-  fill_candidates <- matrix(ncol = 3)
-  x_mins <- matrix(nrow = length(y_seq), ncol = length(z_seq))
-  x_maxs <- matrix(nrow = length(y_seq), ncol = length(z_seq))
+  fill_candidates <- expand.grid(x_seq, y_seq, z_seq)
+  interior_point <- vector(mode = "logical", length = nrow(fill_candidates))
 
-  for (y_idx in seq_along(y_seq)) {
-    for (z_idx in seq_along(z_seq)) {
-      y_val <- y_seq[y_idx]
-      z_val <- z_seq[z_idx]
-      data_red <- data[data[, 2] == y_val & data[, 3] == z_val, ] |>
-        matrix(ncol = ncol(data))
-
-      if (nrow(data_red) == 0) {
-        x_mins[y_idx, z_idx] <- NA
-        x_maxs[y_idx, z_idx] <- NA
-      } else if (nrow(data_red) > 1) {
-        min_val <- min(unique(data_red[, 1]))
-        max_val <- max(unique(data_red[, 1]))
-        if (max_val - min_val > 1) {
-          x_mins[y_idx, z_idx] <- min(unique(data_red[, 1]))
-          x_maxs[y_idx, z_idx] <- max(unique(data_red[, 1]))
-        }
-      } else {
-        x_mins[y_idx, z_idx] <- NA
-        x_maxs[y_idx, z_idx] <- NA
-      }
-    }
+  for (row_idx in seq_len(nrow(fill_candidates))) {
+    interior_point[row_idx] <- check_interior(
+      unlist(fill_candidates[row_idx, ]),
+      data
+    )
   }
 
-  for (y_idx in seq_along(y_seq)) {
-    for (z_idx in seq_along(z_seq)) {
-      y_val <- y_seq[y_idx]
-      z_val <- z_seq[z_idx]
-      data_red <- data[data[, 2] == y_val & data[, 3] == z_val, ] |>
-        matrix(ncol = ncol(data))
-
-      if (nrow(data_red) == 0) {
-        next
-      } else if (nrow(data_red) > 1) {
-        next
-      } else {
-        x_min_vals <- get_surrounding_vals(x_mins, y_idx, z_idx)
-        x_max_vals <- get_surrounding_vals(x_maxs, y_idx, z_idx)
-        if (sum(is.na(x_min_vals)) != length(x_min_vals)) {
-          total_min <- min(x_min_vals, na.rm = TRUE)
-        } else {
-          total_min <- NA
-          next
-        }
-        if (sum(is.na(x_max_vals)) != length(x_max_vals)) {
-          total_max <- max(x_max_vals, na.rm = TRUE)
-        } else {
-          total_max <- NA
-          next
-        }
-        # min_mean <- mean(x_min_vals, na.rm = TRUE)
-        # max_mean <- mean(x_max_vals, na.rm = TRUE)
-        if (abs(data_red[, 1] - total_min) > abs(data_red[, 1] - total_max)) {
-          x_maxs[y_idx, z_idx] <- data_red[, 1]
-        } else {
-          x_mins[y_idx, z_idx] <- data_red[, 1]
-        }
-      }
-    }
-  }
-
-  missing_present <- TRUE
-  n_missing_prev <- nrow(x_mins) * ncol(x_mins)
-  missingness_thresholds <- c(0.25, 0.33, 0.5, 0.66, 0.75, 0.99)
-  threshold_idx <- 1
-
-  while (missing_present == TRUE) {
-    missing_mins <- matrix(nrow = nrow(x_mins), ncol = ncol(x_mins))
-    missing_maxs <- matrix(nrow = nrow(x_maxs), ncol = ncol(x_maxs))
-    n_missing <- 0
-    for (y_idx in seq_along(y_seq)) {
-      for (z_idx in seq_along(z_seq)) {
-        if (is.na(x_mins[y_idx, z_idx])) {
-          data_red <- data[
-            data[, 2] == y_seq[y_idx] & data[, 3] == z_seq[z_idx],
-          ] |>
-            matrix(ncol = ncol(data))
-          if (nrow(data_red) > 0) {
-            n_missing <- n_missing + 1
-            missing_mins[y_idx, z_idx] <- TRUE
-          } else if (!is.na(x_maxs[y_idx, z_idx])) {
-            n_missing <- n_missing + 1
-            missing_mins[y_idx, z_idx] <- TRUE
-          } else {
-            n_present_y_lower <- x_mins[y_idx, 1:z_idx]
-            n_present_y_upper <- x_mins[y_idx, z_idx:length(z_seq)]
-            if ((sum(!is.na(n_present_y_lower)) > 0) && (sum(!is.na(n_present_y_upper)) > 0)) {
-              n_missing <- n_missing + 1
-              missing_mins[y_idx, z_idx] <- TRUE
-            } else {
-              n_present_z_lower <- x_mins[1:y_idx, z_idx]
-              n_present_z_upper <- x_mins[y_idx:length(y_seq), z_idx]
-              if ((sum(!is.na(n_present_z_lower)) > 0) && (sum(!is.na(n_present_z_upper)) > 0)) {
-                n_missing <- n_missing + 1
-                missing_mins[y_idx, z_idx] <- TRUE
-              } else {
-                missing_mins[y_idx, z_idx] <- FALSE
-              }
-            }
-          }
-        } else {
-          missing_mins[y_idx, z_idx] <- FALSE
-        }
-        #   x_min_vals <- get_surrounding_vals(x_mins, y_idx, z_idx)
-        #   num_missing <- sum(is.na(x_min_vals))
-        #   if ((num_missing < 4) | (length(x_min_vals) - num_missing) >= 5) {
-        #     n_missing <- n_missing + 1
-        #     missing_mins[y_idx, z_idx] <- TRUE
-        #   } else {
-        #     missing_mins[y_idx, z_idx] <- FALSE
-        #   }
-        # } else {
-        #   missing_mins[y_idx, z_idx] <- FALSE
-        # }
-        # if (is.na(x_maxs[y_idx, z_idx])) {
-        #   x_max_vals <- get_surrounding_vals(x_maxs, y_idx, z_idx)
-        #   num_missing <- sum(is.na(x_max_vals))
-        #   if ((num_missing < 4) | (length(x_max_vals) - num_missing) >= 5) {
-        #     n_missing <- n_missing + 1
-        #     missing_maxs[y_idx, z_idx] <- TRUE
-        #   } else {
-        #     missing_maxs[y_idx, z_idx] <- FALSE
-        #   }
-        # } else {
-        #   missing_maxs[y_idx, z_idx] <- FALSE
-        # }
-        if (is.na(x_maxs[y_idx, z_idx])) {
-          data_red <- data[data[, 2] == y_seq[y_idx] & data[, 3] == z_seq[z_idx], ] |>
-            matrix(ncol = ncol(data))
-          if (nrow(data_red) > 0) {
-            n_missing <- n_missing + 1
-            missing_maxs[y_idx, z_idx] <- TRUE
-          } else if (!is.na(x_mins[y_idx, z_idx])) {
-            n_missing <- n_missing + 1
-            missing_maxs[y_idx, z_idx] <- TRUE
-          } else {
-            n_present_y_lower <- x_maxs[y_idx, 1:z_idx]
-            n_present_y_upper <- x_maxs[y_idx, z_idx:length(z_seq)]
-            if ((sum(!is.na(n_present_y_lower)) > 0) && (sum(!is.na(n_present_y_upper)) > 0)) {
-              n_missing <- n_missing + 1
-              missing_maxs[y_idx, z_idx] <- TRUE
-            } else {
-              n_present_z_lower <- x_maxs[1:y_idx, z_idx]
-              n_present_z_upper <- x_maxs[y_idx:length(y_seq), z_idx]
-              if ((sum(!is.na(n_present_z_lower)) > 0) && (sum(!is.na(n_present_z_upper)) > 0)) {
-                n_missing <- n_missing + 1
-                missing_maxs[y_idx, z_idx] <- TRUE
-              } else {
-                missing_maxs[y_idx, z_idx] <- FALSE
-              }
-            }
-          }
-        } else {
-          missing_maxs[y_idx, z_idx] <- FALSE
-        }
-      }
-    }
-
-    if (n_missing == 0) {
-      missing_present <- FALSE
-    } else {
-      missing_present <- TRUE
-    }
-
-    if (!(n_missing < n_missing_prev)) {
-      if (threshold_idx < length(missingness_thresholds)) {
-        threshold_idx <- threshold_idx + 1
-      } else {
-        break
-      }
-    }
-    n_missing_prev <- n_missing
-
-    for (y_idx in seq_along(y_seq)) {
-      for (z_idx in seq_along(z_seq)) {
-        if (missing_mins[y_idx, z_idx] == TRUE) {
-          x_min_vals <- get_surrounding_vals(x_mins, y_idx, z_idx)
-          if (sum(is.na(x_min_vals)) / length(x_min_vals) <= missingness_thresholds[threshold_idx]) {
-            # if (sum(!is.na(x_min_vals)) > 1) {
-            # x_mins[y_idx, z_idx] <- min(x_min_vals, na.rm = TRUE)
-            x_mins[y_idx, z_idx] <- round(mean(x_min_vals, na.rm = TRUE))
-            missing_mins[y_idx, z_idx] <- FALSE
-            # n_missing <- n_missing - 1
-          }
-        }
-        if (missing_maxs[y_idx, z_idx] == TRUE) {
-          x_max_vals <- get_surrounding_vals(x_maxs, y_idx, z_idx)
-          if (sum(!is.na(x_max_vals)) / length(x_max_vals) <= missingness_thresholds[threshold_idx]) {
-            # if (sum(!is.na(x_max_vals)) > 1) {
-            # x_maxs[y_idx, z_idx] <- max(x_max_vals, na.rm = TRUE)
-            x_maxs[y_idx, z_idx] <- round(mean(x_max_vals, na.rm = TRUE))
-            missing_maxs[y_idx, z_idx] <- FALSE
-            # n_missing <- n_missing - 1
-          }
-        }
-      }
-    }
-
-    # n_missing <- 0
-    # for (y_idx in 1:length(y_seq)) {
-    #   for (z_idx in 1:length(z_seq)) {
-    #     if (is.na(x_mins[y_idx, z_idx])) {
-    #       x_min_vals <- get_surrounding_vals(x_mins, y_idx, z_idx)
-    #       num_missing <- sum(is.na(x_min_vals))
-    #       if ((num_missing < 4) | (length(x_min_vals) - num_missing) >= 5) {
-    #         n_missing <- n_missing + 1
-    #         missing_mins[y_idx, z_idx] <- TRUE
-    #       } else {
-    #         missing_mins[y_idx, z_idx] <- FALSE
-    #       }
-    #     } else {
-    #       missing_mins[y_idx, z_idx] <- FALSE
-    #     }
-    #     if (is.na(x_maxs[y_idx, z_idx])) {
-    #       x_max_vals <- get_surrounding_vals(x_maxs, y_idx, z_idx)
-    #       num_missing <- sum(is.na(x_max_vals))
-    #       if ((num_missing < 4) | (length(x_max_vals) - num_missing) >= 5) {
-    #         n_missing <- n_missing + 1
-    #         missing_maxs[y_idx, z_idx] <- TRUE
-    #       } else {
-    #         missing_maxs[y_idx, z_idx] <- FALSE
-    #       }
-    #     } else {
-    #       missing_maxs[y_idx, z_idx] <- FALSE
-    #     }
-    #   }
-    # }
-
-    # if (n_missing > 0) {
-    #   missing_present <- TRUE
-    # } else {
-    #   missing_present <- FALSE
-    # }
-    # print(sum(!is.na(x_mins)))
-    # print(sum(!is.na(x_maxs)))
-  }
-
-  volume <- 0
-  for (y_idx in seq_along(y_seq)) {
-    for (z_idx in seq_along(z_seq)) {
-      x_min_val <- x_mins[y_idx, z_idx]
-      x_max_val <- x_maxs[y_idx, z_idx]
-
-      if (!is.na(x_min_val) && !is.na((x_max_val))) {
-        x_vals <- seq(min(x_min_val, x_max_val), max(x_min_val, x_max_val), by = 1)
-        for (x_val in x_vals) {
-          fill_candidates <- rbind(
-            fill_candidates,
-            c(x_val, y_seq[y_idx], z_seq[z_idx])
-          )
-        }
-      }
-    }
-  }
-  fill_candidates <- fill_candidates[-1, ]
-  volume <- nrow(fill_candidates) * voxel_volume
-  return(list(volume, fill_candidates))
+  interior_voxels <- fill_candidates[interior_point, ]
+  est_volume <- sum(interior_point) * voxel_volume
+  volume_out <- list(
+    volume = est_volume,
+    interior_voxels = interior_voxels
+  )
+  return(volume_out)
 }
 
-get_surrounding_vals <- function(mat, x_idx, y_idx) {
-  if (x_idx == 1) {
-    if (y_idx == 1) {
-      out <- mat[x_idx:(x_idx + 1), y_idx:(y_idx + 1)]
-    } else if (y_idx == ncol(mat)) {
-      out <- mat[x_idx:(x_idx + 1), (y_idx - 1):y_idx]
+check_interior <- function(x, data) {
+  data_subsets <- list(
+    matrix(
+      data[near(data[, 2], x[2]) & near(data[, 3], x[3]), ],
+      ncol = ncol(data)
+    ),
+    matrix(
+      data[near(data[, 1], x[1]) & near(data[, 3], x[3]), ],
+      ncol = ncol(data)
+    ),
+    matrix(
+      data[near(data[, 1], x[1]) & near(data[, 2], x[2]), ],
+      ncol = ncol(data)
+    )
+  )
+
+  subset_dims <- map(
+    data_subsets,
+    ~ nrow(.x)
+  ) |>
+    reduce(c)
+
+  interior <- vector(mode = "logical", length = length(subset_dims))
+
+  for (dim_idx in seq_along(data_subsets)) {
+    if (subset_dims[dim_idx] > 0) {
+      if (subset_dims[dim_idx] > 1) {
+        min_val <- min(data_subsets[[dim_idx]][, dim_idx])
+        max_val <- max(data_subsets[[dim_idx]][, dim_idx])
+        if ((x[dim_idx] >= min_val) && (x[dim_idx] <= max_val)) {
+          interior[dim_idx] <- TRUE
+        }
+      } else {
+        val <- max(abs(data_subsets[[dim_idx]][, dim_idx]))
+        if (abs(x[dim_idx]) <= abs(val)) {
+          interior[dim_idx] <- TRUE
+        }
+      }
     } else {
-      out <- mat[x_idx:(x_idx + 1), (y_idx - 1):(y_idx + 1)]
+      interior[dim_idx] <- FALSE
     }
-  } else if (x_idx == nrow(mat)) {
-    if (y_idx == 1) {
-      out <- mat[(x_idx - 1):x_idx, y_idx:(y_idx + 1)]
-    } else if (y_idx == ncol(mat)) {
-      out <- mat[(x_idx - 1):x_idx, (y_idx - 1):y_idx]
-    } else {
-      out <- mat[(x_idx - 1):x_idx, (y_idx - 1):(y_idx + 1)]
-    }
+  }
+
+  if (sum(interior) >= 1) {
+    return(TRUE)
   } else {
-    if (y_idx == 1) {
-      out <- mat[(x_idx - 1):(x_idx + 1), y_idx:(y_idx + 1)]
-    } else if (y_idx == ncol(mat)) {
-      out <- mat[(x_idx - 1):(x_idx + 1), (y_idx - 1):y_idx]
-    } else {
-      out <- mat[(x_idx - 1):(x_idx + 1), (y_idx - 1):(y_idx + 1)]
-    }
+    return(FALSE)
   }
-  out
 }
+
+
---
+++ code/functions/simulations/estimate_volume_case8.R
@@ -0,0 +1,297 @@
+estimate_volume_case8 <- function(sim, n_points) {
+  sim_data <- sim$processed_data$df
+  time_points <- sim_data |>
+    select(time) |>
+    unlist() |>
+    unique()
+
+  observed_times <- sim$data$df |>
+    select(time) |>
+    unlist() |>
+    unique() |>
+    sort()
+
+  sim_mat <- sim$processed_data$df_observed |>
+    select(time, contains("X")) |>
+    as.matrix()
+
+  radii <- map(
+    seq_along(sim$data$amplitude_values),
+    ~ sim$data$amplitude_values[[.x]][1]
+  ) |>
+    reduce(c)
+
+  observed_volumes <- (4 / 3) * pi * radii^3
+
+  time_adjustments <- case_when(
+    time_trend_val == "constant" ~ 0 * time_points,
+    time_trend_val == "linear" ~ time_points,
+    time_trend_val == "quadratic" ~ time_points^2
+  )
+
+# scale time adjustments to be proportion of 1
+  if (max(time_adjustments) == 0) {
+    time_adjustments_scaled <- time_adjustments * time_change_val * 0
+  } else {
+    time_adjustments_scaled <- (
+      time_adjustments / max(time_adjustments)
+    ) * time_change_val
+  }
+# subtract from initial amplitude to replicate effects of atrophy
+  true_radii <- 1 - time_adjustments_scaled
+  true_volumes <- (4 / 3) * pi * true_radii^3 
+
+  ## Calcalate volumes from partitioned models
+
+  lpme_volumes <- vector(mode = "numeric", length = length(time_points))
+  pme_volumes <- vector(mode = "numeric", length = length(time_points))
+
+  lpme_part_volumes <- vector(mode = "numeric", length = length(time_points))
+  pme_part_volumes <- vector(mode = "numeric", length = length(time_points))
+
+  scaling_factors <- sim$data$df |>
+    select(contains("X")) |>
+    select(!contains("true")) |>
+    colMinsMaxs()
+  x_scale <- max(abs(scaling_factors[, 1]))
+  y_scale <- max(abs(scaling_factors[, 2]))
+  z_scale <- max(abs(scaling_factors[, 3]))
+
+
+
+  lpme_est <- sim$models$lpme$lpme
+  lpme_params <- lpme_est$parameterization_list[[which.min(lpme_est$msd)]] 
+
+
+  for (time_idx in seq_along(time_points)) {
+    observed_data <- sim$data$df |>
+      filter(time == observed_times[time_idx]) |>
+      select(contains("X")) |>
+      select(!contains("true")) |>
+      as.matrix()
+    obs_limits <- colMinsMaxs(observed_data)
+    limit_scaler <- 0.05
+    obs_x_range <- obs_limits[2, 1] - obs_limits[1, 1]
+    obs_y_range <- obs_limits[2, 2] - obs_limits[1, 2]
+    obs_z_range <- obs_limits[2, 3] - obs_limits[1, 3]
+
+    obs_x_scale <- max(abs(obs_limits[, 1]))
+    obs_y_scale <- max(abs(obs_limits[, 2]))
+    obs_z_scale <- max(abs(obs_limits[, 3]))
+
+    full_volume <- (obs_x_range * (1 + (2 * limit_scaler))) *
+      (obs_y_range * (1 + (2 * limit_scaler))) *
+      (obs_z_range * (1 + (2 * limit_scaler)))
+
+    temp_data <- sim_mat[sim_mat[, 1] == time_points[time_idx], -1]
+    data_limits <- colMinsMaxs(temp_data)
+
+    x_range <- data_limits[2, 1] - data_limits[1, 1]
+    y_range <- data_limits[2, 2] - data_limits[1, 2]
+    z_range <- data_limits[2, 3] - data_limits[1, 3]
+
+    x_min <- data_limits[1, 1] - (limit_scaler * x_range)
+    x_max <- data_limits[2, 1] + (limit_scaler * x_range)
+    y_min <- data_limits[1, 2] - (limit_scaler * y_range)
+    y_max <- data_limits[2, 2] + (limit_scaler * y_range)
+    z_min <- data_limits[1, 3] - (limit_scaler * z_range)
+    z_max <- data_limits[2, 3] + (limit_scaler * z_range)
+
+    x_candidates <- runif(n = n_points, min = x_min, max = x_max)
+    y_candidates <- runif(n = n_points, min = y_min, max = y_max)
+    z_candidates <- runif(n = n_points, min = z_min, max = z_max)
+
+    candidates <- cbind(x_candidates, y_candidates, z_candidates)
+
+    # Use partitioned models with interior identification
+    temp_pme_part1 <- sim$models$pme_part$pme[[1]][[time_idx]]
+    temp_pme_part2 <- sim$models$pme_part$pme[[2]][[time_idx]]
+    temp_pme_embedding_part1 <- temp_pme_part1$embedding_map
+    temp_pme_embedding_part2 <- temp_pme_part2$embedding_map
+    temp_pme_coefs_part1 <- temp_pme_part1$coefs[[which.min(temp_pme_part1$MSD)]]
+    temp_pme_coefs_part2 <- temp_pme_part2$coefs[[which.min(temp_pme_part2$MSD)]]
+    temp_pme_params_part1 <- temp_pme_part1$parameterization[[which.min(temp_pme_part1$MSD)]]
+    temp_pme_params_part2 <- temp_pme_part2$parameterization[[which.min(temp_pme_part2$MSD)]]
+
+    lpme_est_part1 <- sim$models$lpme_part$lpme[[1]]
+    lpme_est_part2 <- sim$models$lpme_part$lpme[[2]]
+
+    temp_lpme_coefs_part1 <- lpme_est_part1$sol_coef_functions[[
+      which.min(lpme_est_part1$msd)
+    ]](time_points[time_idx])
+    temp_lpme_coefs_part2 <- lpme_est_part2$sol_coef_functions[[
+      which.min(lpme_est_part2$msd)
+    ]](time_points[time_idx])
+
+    temp_lpme_params_part1 <- lpme_est_part1$parameterization_list[[which.min(lpme_est_part1$msd)]]
+    temp_lpme_params_part2 <- lpme_est_part2$parameterization_list[[which.min(lpme_est_part2$msd)]]
+    temp_lpme_params_part1 <- temp_lpme_params_part1[
+      temp_lpme_params_part1[, 1] == time_points[time_idx],
+      -1
+    ]
+    temp_lpme_params_part2 <- temp_lpme_params_part2[
+      temp_lpme_params_part2[, 1] == time_points[time_idx],
+      -1
+    ]
+
+    lpme_n_knots_part1 <- nrow(temp_lpme_params_part1)
+    lpme_n_knots_part2 <- nrow(temp_lpme_params_part2)
+    d <- ncol(temp_lpme_params_part1)
+    coef_mat_part1 <- matrix(
+      temp_lpme_coefs_part1,
+      lpme_n_knots_part1 + d + 1,
+      byrow = TRUE
+    )
+    coef_mat_part2 <- matrix(
+      temp_lpme_coefs_part2,
+      lpme_n_knots_part2 + d + 1,
+      byrow = TRUE
+    )
+
+    temp_lpme_embedding_part1 <- function(r) {
+      t(coef_mat_part1[1:lpme_n_knots_part1, ]) %*% pme::etaFunc(r, temp_lpme_params_part1, 4 - d) +
+        t(coef_mat_part1[(lpme_n_knots_part1 + 1):(lpme_n_knots_part1 + d + 1), ]) %*%
+          matrix(c(1, r), ncol = 1)
+    }
+
+    temp_lpme_embedding_part2 <- function(r) {
+      t(coef_mat_part2[1:lpme_n_knots_part2, ]) %*% pme::etaFunc(r, temp_lpme_params_part2, 4 - d) +
+        t(coef_mat_part2[(lpme_n_knots_part2 + 1):(lpme_n_knots_part2 + d + 1), ]) %*%
+          matrix(c(1, r), ncol = 1)
+    }
+
+    lpme_interior_part1 <- interior_identification(
+      temp_lpme_embedding_part1,
+      coef_mat_part1,
+      temp_lpme_params_part1,
+      candidates[candidates[, 1] > 0, ],
+      c(0, 0, 0)
+    )
+    lpme_interior_part2 <- interior_identification(
+      temp_lpme_embedding_part2,
+      coef_mat_part2,
+      temp_lpme_params_part2,
+      candidates[candidates[, 1] <= 0, ],
+      c(0, 0, 0)
+    )
+
+    lpme_interior <- c(lpme_interior_part1, lpme_interior_part2)
+
+    lpme_interior_candidates <- rbind(
+      candidates[candidates[, 1] > 0, ][lpme_interior_part1, ],
+      candidates[candidates[, 1] <= 0, ][lpme_interior_part2, ]
+    )
+    
+    pme_interior_part1 <- interior_identification(
+      temp_pme_embedding_part1,
+      temp_pme_coefs_part1,
+      temp_pme_params_part1,
+      candidates[candidates[, 1] > 0, ],
+      c(0, 0, 0)
+    )
+    pme_interior_part2 <- interior_identification(
+      temp_pme_embedding_part2,
+      temp_pme_coefs_part2,
+      temp_pme_params_part2,
+      candidates[candidates[, 1] <= 0, ],
+      c(0, 0, 0)
+    )
+
+    pme_interior <- c(pme_interior_part1, pme_interior_part2)
+    pme_interior_candidates <- rbind(
+      candidates[candidates[, 1] > 0, ][pme_interior_part1, ],
+      candidates[candidates[, 1] <= 0, ][pme_interior_part2, ]
+    )
+
+    lpme_part_volumes[time_idx] <- mean(lpme_interior) * full_volume
+    pme_part_volumes[time_idx] <- mean(pme_interior) * full_volume
+
+
+    temp_pme <- sim$models$pme$pme[[time_idx]]
+    temp_pme_params <- temp_pme$parameterization[[which.min(temp_pme$MSD)]]
+
+    temp_lpme_params <- lpme_params[lpme_params[, 1] == time_points[time_idx], -1]
+
+    pme_param_limits <- colMinsMaxs(temp_pme_params)
+    lpme_param_limits <- colMinsMaxs(temp_lpme_params)
+
+    pme_param_candidates1 <- runif(
+      n = n_points, 
+      min = pme_param_limits[1, 1], 
+      max = pme_param_limits[2, 1]
+    )
+    pme_param_candidates2 <- runif(
+      n = n_points, 
+      min = pme_param_limits[1, 2], 
+      max = pme_param_limits[2, 2]
+    )
+    lpme_param_candidates1 <- runif(
+      n = n_points,
+      min = lpme_param_limits[1, 1],
+      max = lpme_param_limits[2, 1]
+    )
+    lpme_param_candidates2 <- runif(
+      n = n_points,
+      min = lpme_param_limits[1, 2],
+      max = lpme_param_limits[2, 2]
+    )
+
+
+    temp_lpme_reconstructions <- map(
+      seq_len(n_points),
+      ~ lpme_est$embedding_map(
+        c(
+          time_points[time_idx], 
+          lpme_param_candidates1[.x], 
+          lpme_param_candidates2[.x]
+        )
+      )
+    ) |>
+      reduce(rbind)
+    temp_lpme_reconstructions <- temp_lpme_reconstructions[, 2:4]
+
+    temp_pme_reconstructions <- map(
+      seq_len(n_points),
+      ~ temp_pme$embedding_map(
+        c(
+          pme_param_candidates1[.x],
+          pme_param_candidates2[.x]
+        )
+      )
+    ) |>
+      reduce(rbind)
+    temp_pme_reconstructions <- temp_pme_reconstructions[, 1:3]
+
+    temp_lpme_reconstructions[, 1] <- temp_lpme_reconstructions[, 1] * x_scale
+    temp_lpme_reconstructions[, 2] <- temp_lpme_reconstructions[, 2] * y_scale
+    temp_lpme_reconstructions[, 3] <- temp_lpme_reconstructions[, 3] * z_scale
+
+    temp_pme_reconstructions[, 1] <- temp_pme_reconstructions[, 1] * x_scale
+    temp_pme_reconstructions[, 2] <- temp_pme_reconstructions[, 2] * y_scale
+    temp_pme_reconstructions[, 3] <- temp_pme_reconstructions[, 3] * z_scale
+
+    temp_lpme_volume <- estimate_volume(
+      round(temp_lpme_reconstructions, 1),
+      voxel_volume = 0.1^3
+    )
+    temp_pme_volume <- estimate_volume(
+      round(temp_pme_reconstructions, 1),
+      voxel_volume = 0.1^3
+    )
+
+    lpme_volumes[time_idx] <- temp_lpme_volume$volume
+    pme_volumes[time_idx] <- temp_pme_volume$volume
+  }
+
+  volume_list <- list(
+    times = observed_times,
+    observed = observed_volumes,
+    true = true_volumes,
+    lpme_augmented = lpme_volumes,
+    pme_augmented = pme_volumes,
+    lpme_part = lpme_part_volumes,
+    pme_part = pme_part_volumes
+  )
+
+  return(volume_list)
+}
