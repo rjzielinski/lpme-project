e71b50c8ed9b1351bba20fbfa084f39ec38eea31
Robert Zielinski <rjz1996@outlook.com>
2025-06-13 22:13:43 -0400

    update adni applicaiton code structure

---
+++ code/functions/fit_adni.R
@@ -0,0 +1,483 @@
+fit_adni <- function(
+  adni_surface,
+  adni_centers,
+  structure,
+  partition_overlap = 0.15,
+  cores = parallel::detectCores()
+) {
+  require(dplyr)
+  require(future)
+  require(pme)
+  require(tictoc)
+  require(tidyr)
+
+  patnos <- unique(adni_surface$subid)
+
+  plan(multisession, workers = cores)
+
+  adni_fit <- list()
+
+  for (patno_idx in seq_along(patnos)) {
+    patno <- patnos[patno_idx]
+    patno_adni <- adni_surface |>
+      filter(subid == patno) |>
+      select(time_from_bl, x, y, z, theta, phi) |>
+      mutate(
+        partition = z > 0,
+        partition1 = z > -partition_overlap,
+        partition2 = z < partition_overlap
+      )
+
+    patno_adni_centers <- adni_centers |>
+      filter(subid == patno) |>
+      group_by(date) |>
+      summarize(
+        max_x = max(max_x),
+        max_y = max(max_y),
+        max_z = max(max_z)
+      )
+
+    adni_aug <- patno_adni |>
+      select(-partition, -partition1, -partition2) |>
+      as.matrix()
+
+    adni_pt1 <- patno_adni |>
+      filter(partition1 == TRUE) |>
+      select(time_from_bl, x, y, z) |>
+      as.matrix()
+    adni_pt2 <- patno_adni |>
+      filter(partition2 == TRUE) |>
+      select(time_from_bl, x, y, z) |>
+      as.matrix()
+
+    time_values <- unique(patno_adni$time_from_bl)
+
+    adni_fit[[patno_idx]] <- future(
+      {
+        adni_pme_aug_list <- list()
+        adni_pme_aug_time_list <- list()
+        adni_pme_aug_reconstruction_list <- list()
+
+        adni_lpme_part <- replicate(2, NULL, simplify = FALSE)
+        adni_pme_part <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+        adni_pc_part <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+
+        adni_lpme_part_reconstructions <- replicate(2, NULL, simplify = FALSE)
+        adni_pme_part_reconstructions <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+        adni_pc_part_reconstructions <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+
+        adni_lpme_part_times <- replicate(2, NULL, simplify = FALSE)
+        adni_pme_part_times <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+        adni_pc_part_times <- replicate(
+          2,
+          replicate(length(time_values), NULL, simplify = FALSE),
+          simplify = FALSE
+        )
+
+        tic()
+        adni_lpme_aug <- lpme(
+          adni_aug,
+          d = 2,
+          verbose = TRUE,
+          print_plots = FALSE
+        )
+        adni_lpme_aug_time <- toc()
+        adni_lpme_aug_time <- adni_lpme_aug_time$toc - adni_lpme_aug_time$tic
+
+        adni_lpme_aug_reconstructions <- calculate_lpme_reconstructions(
+          adni_lpme_aug,
+          adni_aug
+        )
+
+        tic()
+        adni_lpme_part[[1]] <- lpme(
+          adni_pt1,
+          d = 2,
+          verbose = FALSE,
+          print_plots = FALSE
+        )
+        adni_lpme_time_pt1 <- toc()
+        adni_lpme_part_times[[1]] <- adni_lpme_time_pt1$toc -
+          adni_lpme_time_pt1$tic
+        adni_lpme_part_reconstructions[[1]] <- calculate_lpme_reconstructions(
+          adni_lpme_part[[1]],
+          adni_pt1[adni_pt1[, 4] > 0, ]
+        )
+        tic()
+        adni_lpme_part[[2]] <- lpme(
+          adni_pt2,
+          d = 2,
+          verbose = FALSE,
+          print_plots = FALSE
+        )
+        adni_lpme_time_pt2 <- toc()
+        adni_lpme_part_times[[2]] <- adni_lpme_time_pt2$toc -
+          adni_lpme_time_pt2$tic
+        adni_lpme_part_reconstructions[[2]] <- calculate_lpme_reconstructions(
+          adni_lpme_part[[2]],
+          adni_pt2[adni_pt2[, 4] <= 0, ]
+        )
+
+        for (time_idx in seq_along(time_values)) {
+          temp_adni <- adni_aug[adni_aug[, 1] == time_values[time_idx], -1]
+          temp_adni_pt1 <- adni_pt1[
+            adni_pt1[, 1] == time_values[time_idx],
+            -1
+          ]
+          temp_adni_pt2 <- adni_pt2[
+            adni_pt2[, 1] == time_values[time_idx],
+            -1
+          ]
+
+          tic()
+          adni_pme_aug_list[[time_idx]] <- pme(temp_adni, d = 2)
+          temp_pme_aug_time <- toc()
+          adni_pme_aug_time_list[[time_idx]] <- temp_pme_aug_time$toc -
+            temp_pme_aug_time$tic
+          adni_pme_aug_reconstruction_list[[
+            time_idx
+          ]] <- calculate_pme_reconstructions(
+            adni_pme_aug_list[[time_idx]],
+            temp_adni
+          )
+          adni_pme_aug_reconstruction_list[[time_idx]] <- cbind(
+            time_values[time_idx],
+            adni_pme_aug_reconstruction_list[[time_idx]]
+          )
+
+          tic()
+          adni_pme_part[[1]][[time_idx]] <- pme(temp_adni_pt1, d = 2)
+          temp_pme_time_pt1 <- toc()
+          adni_pme_part_times[[1]][[time_idx]] <- temp_pme_time_pt1$toc -
+            temp_pme_time_pt1$tic
+          adni_pme_part_reconstructions[[1]][[
+            time_idx
+          ]] <- calculate_pme_reconstructions(
+            adni_pme_part[[1]][[time_idx]],
+            temp_adni_pt1[temp_adni_pt1[, 3] > 0, ]
+          )
+          tic()
+          adni_pme_part[[2]][[time_idx]] <- pme(temp_adni_pt2, d = 2)
+          temp_pme_time_pt2 <- toc()
+          adni_pme_part_times[[2]][[time_idx]] <- temp_pme_time_pt2$toc -
+            temp_pme_time_pt2$tic
+          adni_pme_part_reconstructions[[2]][[
+            time_idx
+          ]] <- calculate_pme_reconstructions(
+            adni_pme_part[[2]][[time_idx]],
+            temp_adni_pt2[temp_adni_pt2[, 3] <= 0, ]
+          )
+
+          adni_pme_part_reconstructions[[1]][[time_idx]] <- cbind(
+            time_values[time_idx],
+            adni_pme_part_reconstructions[[1]][[time_idx]]
+          )
+          adni_pme_part_reconstructions[[2]][[time_idx]] <- cbind(
+            time_values[time_idx],
+            adni_pme_part_reconstructions[[2]][[time_idx]]
+          )
+
+          tic()
+          principal_surface_part1 <- prinSurf(temp_adni_pt1)
+          temp_pc_time_pt1 <- toc()
+          adni_pc_part_times[[1]][[time_idx]] <- temp_pc_time_pt1$toc -
+            temp_pc_time_pt1$tic
+          surface_mse_part1 <- map(
+            seq_along(principal_surface_part1),
+            ~ principal_surface_part1[[.x]]$MSE
+          ) |>
+            unlist()
+          opt_surface_part1 <- which.min(surface_mse_part1)
+
+          adni_pc_part[[1]][[time_idx]] <- principal_surface_part1[[
+            opt_surface_part1 + 2
+          ]]
+          adni_pc_part_reconstructions[[1]][[time_idx]] <- cbind(
+            time_values[time_idx],
+            principal_surface_part1[[opt_surface_part1 + 2]]$PS
+          )
+          adni_pc_part_reconstructions[[1]][[
+            time_idx
+          ]] <- adni_pc_part_reconstructions[[1]][[time_idx]][
+            temp_adni_pt1[, 3] > 0,
+          ]
+
+          tic()
+          principal_surface_part2 <- prinSurf(temp_adni_pt2)
+          temp_pc_time_pt2 <- toc()
+          adni_pc_part_times[[2]][[time_idx]] <- temp_pc_time_pt2$toc -
+            temp_pc_time_pt2$tic
+          surface_mse_part2 <- map(
+            seq_along(principal_surface_part2),
+            ~ principal_surface_part2[[.x]]$MSE
+          ) |>
+            unlist()
+          opt_surface_part2 <- which.min(surface_mse_part2)
+
+          adni_pc_part[[2]][[time_idx]] <- principal_surface_part2[[
+            opt_surface_part2 + 2
+          ]]
+          adni_pc_part_reconstructions[[2]][[time_idx]] <- cbind(
+            time_values[time_idx],
+            principal_surface_part2[[opt_surface_part2 + 2]]$PS
+          )
+          adni_pc_part_reconstructions[[2]][[
+            time_idx
+          ]] <- adni_pc_part_reconstructions[[2]][[time_idx]][
+            temp_adni_pt2[, 3] <= 0,
+          ]
+        }
+
+        adni_lpme_part_reconstructions <- reduce(
+          adni_lpme_part_reconstructions,
+          rbind
+        )
+
+        adni_lpme_part_times <- reduce(
+          adni_lpme_part_times,
+          sum
+        )
+
+        adni_pme_aug_reconstructions <- reduce(
+          adni_pme_aug_reconstruction_list,
+          rbind
+        )
+
+        adni_pme_aug_time_list <- reduce(
+          adni_pme_aug_time_list,
+          sum
+        )
+
+        adni_pme_part_reconstructions <- map(
+          seq_along(adni_pme_part_reconstructions),
+          ~ reduce(adni_pme_part_reconstructions[[.x]], rbind)
+        )
+        adni_pme_part_reconstructions <- reduce(
+          adni_pme_part_reconstructions,
+          rbind
+        )
+
+        adni_pme_part_times <- map(
+          seq_along(adni_pme_part_times),
+          ~ reduce(adni_pme_part_times[[.x]], sum)
+        )
+        adni_pme_part_times <- reduce(
+          adni_pme_part_times,
+          sum
+        )
+
+        adni_pc_part_reconstructions <- map(
+          seq_along(adni_pc_part_reconstructions),
+          ~ reduce(adni_pc_part_reconstructions[[.x]], rbind)
+        )
+        adni_pc_part_reconstructions <- reduce(
+          adni_pc_part_reconstructions,
+          rbind
+        )
+
+        adni_pc_part_times <- map(
+          seq_along(adni_pc_part_times),
+          ~ reduce(adni_pc_part_times[[.x]], sum)
+        )
+        adni_pc_part_times <- reduce(
+          adni_pc_part_times,
+          sum
+        )
+
+        adni_lpme_part_volumes <- estimate_volume_interior_lpme(
+          adni_lpme_part,
+          list(adni_pt1, adni_pt2),
+          time_values,
+          n_points = 10000,
+          data_max = patno_adni_centers,
+          limit_scaler = 0.05,
+          partition_index = 3
+        )
+        adni_lpme_part_volumes <- adni_lpme_part_volumes$volumes
+
+        adni_pme_part_volumes <- estimate_volume_interior_pme(
+          adni_pme_part,
+          list(adni_pt1, adni_pt2),
+          time_values,
+          n_points = 10000,
+          data_max = patno_adni_centers,
+          limit_scaler = 0.05,
+          partition_index = 3
+        )
+        adni_pme_part_volumes <- adni_pme_part_volumes$volumes
+
+        adni_lpme_aug_volumes <- vector()
+        adni_pme_aug_volumes <- vector()
+        adni_pc_part_volumes <- vector()
+
+        adni_lpme_aug_interior <- list()
+        adni_pme_aug_interior <- list()
+        adni_pc_part_interior <- list()
+
+        adni_lpme_part_volumes_mesh <- vector()
+        adni_pme_part_volumes_mesh <- vector()
+
+        for (time_idx in seq_along(time_values)) {
+          x_scale <- patno_adni_centers$max_x[time_idx]
+          y_scale <- patno_adni_centers$max_y[time_idx]
+          z_scale <- patno_adni_centers$max_z[time_idx]
+          temp_lpme_reconstructions <- adni_lpme_aug_reconstructions[
+            adni_lpme_aug_reconstructions[, 1] == time_values[time_idx],
+            2:4
+          ]
+          temp_pme_reconstructions <- adni_pme_aug_reconstructions[
+            adni_pme_aug_reconstructions[, 1] == time_values[time_idx],
+            2:4
+          ]
+          temp_pc_reconstructions <- adni_pc_part_reconstructions[
+            adni_pc_part_reconstructions[, 1] == time_values[time_idx],
+            2:4
+          ]
+
+          temp_lpme_part_reconstructions <- adni_lpme_part_reconstructions[
+            adni_lpme_part_reconstructions[, 1] == time_values[time_idx],
+            2:4
+          ]
+          temp_pme_part_reconstructions <- adni_pme_part_reconstructions[
+            adni_pme_part_reconstructions[, 1] == time_values[time_idx],
+            2:4
+          ]
+
+          temp_lpme_ashape <- ashape3d(
+            temp_lpme_reconstructions,
+            alpha = seq(0.5, 1.5, by = 0.1)
+          )
+          adni_lpme_aug_volumes[time_idx] <- volume_ashape3d(
+            temp_lpme_ashape,
+            indexAlpha = "all"
+          ) |>
+            median() *
+            (x_scale * y_scale * z_scale)
+
+          temp_pme_ashape <- ashape3d(
+            temp_pme_reconstructions,
+            alpha = seq(0.5, 1.5, by = 0.1)
+          )
+          adni_pme_aug_volumes[time_idx] <- volume_ashape3d(
+            temp_pme_ashape,
+            indexAlpha = "all"
+          ) |>
+            median() *
+            (x_scale * y_scale * z_scale)
+
+          temp_pc_ashape <- ashape3d(
+            temp_pc_reconstructions,
+            alpha = seq(0.5, 1.5, by = 0.1)
+          )
+          adni_pc_part_volumes[time_idx] <- volume_ashape3d(
+            temp_pc_ashape,
+            indexAlpha = "all"
+          ) |>
+            median() *
+            (x_scale * y_scale * z_scale)
+
+          temp_lpme_part_ashape <- ashape3d(
+            temp_lpme_part_reconstructions,
+            alpha = seq(0.5, 1.5, by = 0.1)
+          )
+          adni_lpme_part_volumes_mesh[time_idx] <- volume_ashape3d(
+            temp_lpme_part_ashape,
+            indexAlpha = "all"
+          ) |>
+            median() *
+            (x_scale * y_scale * z_scale)
+
+          temp_pme_part_ashape <- ashape3d(
+            temp_pme_part_reconstructions,
+            alpha = seq(0.5, 1.5, by = 0.1)
+          )
+          adni_pme_part_volumes_mesh[time_idx] <- volume_ashape3d(
+            temp_pme_part_ashape,
+            indexAlpha = "all"
+          ) |>
+            median() *
+            (x_scale * y_scale * z_scale)
+        }
+
+        adni_lpme_aug_out <- list(
+          lpme = adni_lpme_aug,
+          reconstructions = adni_lpme_aug_reconstructions,
+          volumes = adni_lpme_aug_volumes,
+          fit_time = adni_lpme_aug_time
+        )
+        adni_pme_aug_out <- list(
+          pme = adni_pme_aug_list,
+          reconstructions = adni_pme_aug_reconstructions,
+          volumes = adni_pme_aug_volumes,
+          fit_time = adni_pme_aug_time_list
+        )
+
+        adni_lpme_part_out <- list(
+          lpme = adni_lpme_part,
+          reconstructions = adni_lpme_part_reconstructions,
+          volumes = adni_lpme_part_volumes,
+          volumes_mesh = adni_lpme_part_volumes_mesh,
+          fit_time = adni_lpme_part_times
+        )
+
+        adni_pme_part_out <- list(
+          pme = adni_pme_part,
+          reconstructions = adni_pme_part_reconstructions,
+          volumes = adni_pme_part_volumes,
+          volumes_mesh = adni_pme_part_volumes_mesh,
+          fit_time = adni_pme_part_times
+        )
+
+        adni_pc_part_out <- list(
+          pc = adni_pc_part,
+          reconstructions = adni_pc_part_reconstructions,
+          volumes = adni_pc_part_volumes,
+          fit_time = adni_pc_part_times
+        )
+
+        adni <- list(
+          lpme_aug = adni_lpme_aug_out,
+          pme_aug = adni_pme_aug_out,
+          lpme_part = adni_lpme_part_out,
+          pme_part = adni_pme_part_out,
+          pc_part = adni_pc_part_out
+        )
+
+        if (!dir.exists(here(paste0("output/adni/", patno)))) {
+          dir.create(here(paste0("output/adni/", patno)), recursive = TRUE)
+        }
+
+        saveRDS(
+          adni,
+          here(paste0("output/adni/", patno, "/", structure, "_results.RDS"))
+        )
+
+        TRUE
+      },
+      seed = TRUE
+    )
+  }
+
+  map(adni_fit, value) |>
+    reduce(c)
+}
---
+++ code/functions/preprocess_adni.R
@@ -0,0 +1,53 @@
+preprocess_adni <- function(adni_surface, adni_info, min_duration = 2) {
+  require(dplyr)
+  require(lubridate)
+  require(tidyr)
+
+  adni_surface <- adni_surface |>
+    left_join(adni_info, by = join_by(scan_id == image_id))
+
+  adni_surface <- adni_surface |>
+    mutate(date = decimal_date(date))
+
+  adni_centers <- adni_surface |>
+    group_by(subid, date, scan_id) |>
+    summarise(
+      mean_x = mean(x),
+      mean_y = mean(y),
+      mean_z = mean(z),
+      max_x = max(abs(x - mean_x)),
+      max_y = max(abs(y - mean_y)),
+      max_z = max(abs(z - mean_z))
+    ) |>
+    ungroup()
+
+  adni_bl <- adni_surface |>
+    group_by(subid) |>
+    arrange(date) |>
+    summarize(
+      date_bl = first(date),
+      date_max = max(date)
+    ) |>
+    mutate(duration = date_max - date_bl)
+
+  adni_surface <- adni_surface |>
+    left_join(adni_centers, by = c("subid", "date", "scan_id")) |>
+    left_join(adni_bl, by = "subid") |>
+    mutate(
+      x = (x - mean_x) / max_x,
+      y = (y - mean_y) / max_y,
+      z = (z - mean_z) / max_z,
+      time_from_bl = (date - date_bl) / duration
+    ) |>
+    filter(duration > min_duration)
+
+  adni_surface_spherical <- adni_surface |>
+    select(x, y, z) |>
+    as.matrix() |>
+    cart2sph() |>
+    as_tibble()
+
+  adni_surface <- bind_cols(adni_surface, adni_surface_spherical)
+
+  list(surface = adni_surface, centers = adni_centers)
+}
