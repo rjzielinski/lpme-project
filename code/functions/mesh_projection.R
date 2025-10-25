cd3bf353ae0683e1c8905930497252c8ea980384
Robert Zielinski <rjz1996@outlook.com>
2025-10-24 23:17:27 -0400

    allow projection to different axes

--- code/functions/mesh_projection.R
+++ code/functions/mesh_projection.R
@@ -1,13 +1,18 @@
-mesh_projection <- function(data) {
+mesh_projection <- function(data, axis = 3) {
   require("reticulate")
   use_condaenv("lpme")
   pv <- import("pyvista")
 
   cloud <- pv$PolyData(data)
 
-  origin = cloud$center
-  origin[[length(origin)]] <- cloud$length / 3.0
-  projected <- cloud$project_points_to_plane(origin = origin)
+  origin <- cloud$center
+  normal <- rep(0, 3)
+  normal[axis] <- 1
+  # origin[[length(origin)]] <- cloud$length / 3.0
+  projected <- cloud$project_points_to_plane(
+    origin = origin,
+    normal = normal
+  )
 
   out_mesh <- projected$delaunay_2d(alpha = 0.25)
   edges <- out_mesh$extract_feature_edges(
